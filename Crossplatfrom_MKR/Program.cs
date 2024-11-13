5f2d94607a6dfb3ae36a164d40fbab590e0394db
sashaboiko7 <sashaboiko777@gmail.com>
2024-11-13 09:52:19 +0200

    add input.txt file, update .gitignore

--- Crossplatfrom_MKR/Program.cs
+++ Crossplatfrom_MKR/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Crossplatform_MKR;
 
-string input = File.ReadAllText(@"INPUT.TXT").Trim();
+string input = File.ReadAllText(@"..\..\..\INPUT.TXT").Trim();
 int[] parts = input.Split(' ').Select(int.Parse).ToArray();
 
 int a = parts[0];
@@ -9,5 +9,5 @@ int c = parts[2];
 
 string result = PermutationSolver.Solve(a, b, c);
 
-using var writer = new StreamWriter(@"OUTPUT.TXT");
+using var writer = new StreamWriter(@"..\..\..\OUTPUT.TXT");
 writer.WriteLine(result);
